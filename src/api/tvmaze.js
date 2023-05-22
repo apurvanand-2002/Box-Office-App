fefaffacc1f695bd54d11c7ba0e29582b9d9efac
Apurv Anand <apurvanand.2002@gmail.com>
2023-05-22 18:42:03 +0530

    Added search for actors.

--- src/api/tvmaze.js
+++ src/api/tvmaze.js
@@ -1,8 +1,9 @@
 const BASE_URL = 'https://api.tvmaze.com';
 const apiGet = async queryString => {
-  const response = await fetch(`${BASE_URL}${queryString}`);
-  const body = await response.json();
-  return body;
+    const response = await fetch(`${BASE_URL}${queryString}`);
+    const body = await response.json();
+    return body;
 };
 
 export const searchForShows = query => apiGet(`/search/shows?q=${query}`);
+export const searchForPeople = query => apiGet(`/search/people?q=${query}`);
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,57 +1,81 @@
 import { useState } from 'react';
-import { searchForShows } from './../api/tvmaze';
+import { searchForShows, searchForPeople } from './../api/tvmaze';
 
 const Home = () => {
-  const [searchStr, setsearchStr] = useState('');
-  const [apiData, setApiData] = useState(null);
-  const [apiDataError, setApiDataError] = useState(null);
-  console.log(apiDataError);
+    const [searchStr, setsearchStr] = useState('');
+    const [apiData, setApiData] = useState(null);
+    const [apiDataError, setApiDataError] = useState(null);
+    const [searchOption, setSearchOption] = useState('shows');
 
-  const onSearchInputChange = ev => {
-    setsearchStr(ev.target.value);
-  };
-  const onSearch = async ev => {
-    ev.preventDefault();
-    try {
-      setApiDataError(null);
-      const result = await searchForShows(searchStr);
-      setApiData(result);
-    } catch (error) {
-      setApiDataError(error);
+    const onSearchInputChange = ev => {
+        setsearchStr(ev.target.value);
+    };
+
+    const onRadioChange = (ev) => {
+        setSearchOption(ev.target.value);
     }
 
-    //searchForShows(searchStr)
-    // const body = await apiGet(`/search/shows?q=${searchStr}`);
+    const onSearch = async ev => {
+        ev.preventDefault();
+        try {
+            setApiDataError(null);
+            if (searchOption === 'shows') {
+                const result = await searchForShows(searchStr);
+                setApiData(result);
+            } else {
+                const result = await searchForPeople(searchStr);
+                setApiData(result);
+            }
 
-    // const response = await fetch(`https://api.tvmaze.com/search/shows?q=${searchStr}`)
-    // const body = await response.json();
-    // console.log(body);
-    // // https://api.tvmaze.com/search/shows?q=boys
-  };
+        } catch (error) {
+            setApiDataError(error);
+        }
 
-  const renderApiData = () => {
-    if (apiDataError) {
-      return <div>Error occured: {apiDataError.message}</div>;
-    }
-    if (apiData) {
-      return apiData.map(data => (
-        <div key={data.show.id}>{data.show.name}</div>
-      ));
-    }
-    return null;
-    // {
-    //     apiData.map(data => <div key={data.show.id}>{data.show.name}</div>)
-    // }
-  };
-  return (
-    <div>
-      <form onSubmit={onSearch}>
-        <input type="text" value={searchStr} onChange={onSearchInputChange} />
-        <button type="submit">Search</button>
-      </form>
+        //searchForShows(searchStr)
+        // const body = await apiGet(`/search/shows?q=${searchStr}`);
 
-      <div>{renderApiData()}</div>
-    </div>
-  );
+        // const response = await fetch(`https://api.tvmaze.com/search/shows?q=${searchStr}`)
+        // const body = await response.json();
+        // console.log(body);
+        // // https://api.tvmaze.com/search/shows?q=boys
+    };
+
+    const renderApiData = () => {
+        if (apiDataError) {
+            return <div>Error occured: {apiDataError.message}</div>;
+        }
+        if (apiData) {
+            return apiData[0].show ? apiData.map(data => (
+                <div key={data.show.id}>{data.show.name}</div>
+            )) : apiData.map(data => (
+                <div key={data.person.id}>{data.person.name}</div>
+            ));
+        }
+        return null;
+        // {
+        //     apiData.map(data => <div key={data.show.id}>{data.show.name}</div>)
+        // }
+    };
+    return (
+        <div>
+            <form onSubmit={onSearch}>
+                <input type="text" value={searchStr} onChange={onSearchInputChange} />
+
+                <label>
+                    Shows
+                    <input type='radio' name='search-option' value="shows" checked={searchOption === 'shows'} onChange={onRadioChange} />
+                </label>
+
+                <label>
+                    Actors
+                    <input type='radio' name='search-option' value="actors" checked={searchOption === 'actors'} onChange={onRadioChange} />
+                </label>
+
+                <button type="submit">Search</button>
+            </form>
+
+            <div>{renderApiData()}</div>
+        </div>
+    );
 };
 export default Home;
