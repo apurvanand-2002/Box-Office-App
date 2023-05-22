05bb111a594cb2c9dee7ad405a8ba91a4fdbc239
Apurv Anand <apurvanand.2002@gmail.com>
2023-05-22 15:58:20 +0530

    Fetched TV MAZE API using fetch function & used async & await to get its JSON

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,23 @@
-import { Link } from 'react-router-dom';
+import { useState } from "react";
 
 const Home = () => {
+    const [searchStr, setsearchStr] = useState("");
+
+    const onSearchInputChange = (ev) => {
+        setsearchStr(ev.target.value);
+    }
+    const onSearch = async (ev) => {
+        ev.preventDefault();
+        const response = await fetch(`https://api.tvmaze.com/search/shows?q=${searchStr}`)
+        const body = await response.json();
+        console.log(body);
+        // https://api.tvmaze.com/search/shows?q=boys
+    }
     return (<div>
-        Home
+        <form onSubmit={onSearch}>
+            <input type="text" value={searchStr} onChange={onSearchInputChange} />
+            <button type="submit">Search</button>
+        </form>
     </div>)
 }
 export default Home;
