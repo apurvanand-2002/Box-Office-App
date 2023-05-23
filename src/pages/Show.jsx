614d7833af2db5f25ca48c9b48d3d97d098cbdcf
Apurv Anand <apurvanand.2002@gmail.com>
2023-05-23 09:51:21 +0530

    used data fetching libraries like react query to overcome double fetching of dom elements.

--- src/pages/Show.jsx
+++ src/pages/Show.jsx
@@ -1,29 +1,36 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { getShowById } from '../api/tvmaze';
+import { useQuery } from '@tanstack/react-query'
 
-const useShowById = (showId) => {
-    const [showData, setShowData] = useState(null);
-    const [showError, setShowError] = useState(null);
+// The commented code is optimized using Data fetching libraries like 'Tanstack Query' etc.
+// We can also use '<react.StrictMode> </react.StrictMode>' tag now.
+// const useShowById = (showId) => {
+//     const [showData, setShowData] = useState(null);
+//     const [showError, setShowError] = useState(null);
 
-    useEffect(() => {
-        async function fetchData() {
-            try {
-                const data = await getShowById(showId);
-                setShowData(data);
-            } catch (err) {
-                setShowError(err);
-            }
-        }
-        fetchData();
-    }, [showId])
-    return { showData, showError };
+//     useEffect(() => {
+//         async function fetchData() {
+//             try {
+//                 const data = await getShowById(showId);
+//                 setShowData(data);
+//             } catch (err) {
+//                 setShowError(err);
+//             }
+//         }
+//         fetchData();
+//     }, [showId])
+//     return { showData, showError };
 
-}
+// }
 
 const Show = () => {
     const { showId } = useParams();
-    const { showData, showError } = useShowById(showId);
+    // const { showData, showError } = useShowById(showId);
+    const { data: showData, error: showError } = useQuery({
+        queryKey: ['show', showId],
+        queryFn: () => getShowById(showId)
+    })
 
     if (showError) {
         return <div>We have an error: {showError.message}</div>
